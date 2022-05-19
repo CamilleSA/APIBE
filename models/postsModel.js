aa3f1d609fb9ad9306b48062e64a8e6c3edf563f
Camille <camille.sayous@epitech.eu>
2022-05-19 02:12:26 +0200

    [ADD] db and server

---
+++ models/postsModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const PostsModel = mongoose.model(
+    "node-api",
+    {
+        author: {
+            type: String,
+            required: true
+        },
+        message: {
+            type: String,
+            required: true
+        },
+        data: {
+            type: Date,
+            default: Date.now
+        }
+    },
+    "posts"
+);
+
+module.exports = { PostsModel };
